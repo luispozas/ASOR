a68d4d23a0186b2c07b4e445e91481d6edfbb034
Luis Pozas <51996547+luispozas@users.noreply.github.com>
2021-01-19 18:19:25 +0100

    Update ej1.c

--- Codigos_SO/Practica5_Sockets/ej1.c
+++ Codigos_SO/Practica5_Sockets/ej1.c
@@ -13,14 +13,14 @@ int main(int argc, char **argv){
 	char host[NI_MAXHOST];
    	char serv[NI_MAXSERV];
 
-    memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_UNSPEC; //AF_INET para ipv4
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC; //AF_INET para ipv4
 
-    int rc = getaddrinfo(argv[1], NULL, &hints, &result);
+	int rc = getaddrinfo(argv[1], NULL, &hints, &result);
 
-    for(ptr = result; ptr != NULL; ptr = ptr->ai_next){
-        getnameinfo(ptr->ai_addr, ptr->ai_addrlen, host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
-        printf("%s -- %i -- %i\n", host, ptr->ai_family, ptr->ai_socktype);
-    }
+	for(ptr = result; ptr != NULL; ptr = ptr->ai_next){
+		getnameinfo(ptr->ai_addr, ptr->ai_addrlen, host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+		printf("%s -- %i -- %i\n", host, ptr->ai_family, ptr->ai_socktype);
+	}
 	return 0;
 }
